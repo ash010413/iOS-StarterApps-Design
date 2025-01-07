a595fa9130bcf978fcd325eb60b1c2db5bf41c7b
Ashwinkumar Venkatnarayanan <79954204+ash010413@users.noreply.github.com>
2025-01-07 10:55:12 +0530

    Add files via upload

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Ashwin Kumar on 03/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
